16702089a0ff8c8bd14dd88e803028573ba26e4c
carcigenicate <slomifier@gmail.com>
2017-04-20 15:09:22 -0600

    Added the stripped down main from my BrainFuck interpreter.

--- src/ezfuck/main.clj
+++ src/ezfuck/main.clj
@@ -1,7 +1,20 @@
 (ns ezfuck.main
-  (:require [ezfuck.interpreter :as i])
+  (:require [ezfuck.interpreter :as i]
+            [clojure.string :as string])
 
   (:gen-class))
 
-(defn -main [& args]
-  (i/repl))
+(defn -main [^String mode & [^String path?]]
+  (let [mode-lower (string/lower-case mode)]
+
+    (case (first mode-lower)
+      \i (let [code (if path? (slurp path?) nil)]
+           (if path?
+             (i/interpret code)
+             (println "Enter a path to read from.")))
+
+      \r (i/repl)
+
+      (do
+        (println "To interpret a file, type: i <path>")
+        (println "To start the REPL, type: r")))))
